7a5eff44834ed4a65d5d039cc3976f5bb0f3a22d
Pradeep Nayak <pradeep@pradeep-desktop.(none)>
2012-08-23 16:59:12 +0530

    MTE-4

--- CRM/Mte/Page/callback.php
+++ CRM/Mte/Page/callback.php
@@ -35,61 +35,62 @@ class CRM_Mte_Page_callback extends CRM_Core_Page {
       $bounceType = array();
       $reponse = json_decode($_POST['mandrill_events'], TRUE);
       if (is_array($reponse)) {
+        $events = array('open','click','hard_bounce','soft_bounce','spam','reject');
         foreach ($reponse as $value) {
-          $params = array( 
-            'email' => $value['msg']['email'],
-            'version' => 3,
-          );
-
-          $result = civicrm_api( 'email','get',$params );
-      
-          $mail = new CRM_Mailing_DAO_Mailing();
-          $mail->domain_id = CRM_Core_Config::domainID();
-          $mail->subject = "***All Transactional Emails***";
-          $mail->url_tracking = TRUE;
-          $mail->forward_replies = FALSE;
-          $mail->auto_responder = FALSE;
-          $mail->open_tracking = TRUE;
-
-          if ($mail->find(TRUE) && CRM_Utils_Array::value('values', $result) && !empty($result['values'])){
-            $emails = reset($result['values']);
-            $params = array(
-              'job_id' => CRM_Core_DAO::getFieldValue('CRM_Mailing_DAO_Job', $mail->id, 'id', 'mailing_id'),
-              'contact_id' => $emails['contact_id'],
-              'email_id' => $emails['id'],
-              'activity_id' => CRM_Utils_Array::value('metadata', $value['msg']) ? CRM_Utils_Array::value('CiviCRM_Mandrill_id', $value['msg']['metadata']) : null
+          if (in_array($value['event'], $events)) {
+            $params = array( 
+              'email' => $value['msg']['email'],
+              'version' => 3,
             );
-            $eventQueue = CRM_Mailing_Event_BAO_Queue::create($params);
-          }
-          switch ($value['event']) {
-          case 'open':
-            $oe                 = new CRM_Mailing_Event_BAO_Opened();
-            $oe->event_queue_id = $eventQueue->id;
-            $oe->time_stamp     = date('YmdHis', $value['ts']);
-            $oe->save();
-            break;
-
-          case 'click':
-            $tracker = new CRM_Mailing_BAO_TrackableURL();
-            $tracker->url = $value['url'];
-            $tracker->mailing_id = $mail->id;
-            if (!$tracker->find(TRUE)) {
-              $tracker->save();
-            }
-            $open = new CRM_Mailing_Event_BAO_TrackableURLOpen();
-            $open->event_queue_id = $eventQueue->id;
-            $open->trackable_url_id = $tracker->id;
-            $open->time_stamp = date('YmdHis', $value['ts']);
-            $open->save();
-            break;
+            $result = civicrm_api( 'email','get',$params );
             
-          case 'hard_bounce':
-          case 'soft_bounce':
-          case 'spam':
-          case 'reject':
-            if (empty($bounceType)) {
-              CRM_Core_PseudoConstant::populate($bounceType, 'CRM_Mailing_DAO_BounceType', TRUE, 'id', NULL, NULL, NULL, 'name');
+            $mail = new CRM_Mailing_DAO_Mailing();
+            $mail->domain_id = CRM_Core_Config::domainID();
+            $mail->subject = "***All Transactional Emails***";
+            $mail->url_tracking = TRUE;
+            $mail->forward_replies = FALSE;
+            $mail->auto_responder = FALSE;
+            $mail->open_tracking = TRUE;
+
+            if ($mail->find(TRUE) && CRM_Utils_Array::value('values', $result) && !empty($result['values'])){
+              $emails = reset($result['values']);
+              $params = array(
+                'job_id' => CRM_Core_DAO::getFieldValue('CRM_Mailing_DAO_Job', $mail->id, 'id', 'mailing_id'),
+                'contact_id' => $emails['contact_id'],
+                'email_id' => $emails['id'],
+                'activity_id' => CRM_Utils_Array::value('metadata', $value['msg']) ? CRM_Utils_Array::value('CiviCRM_Mandrill_id', $value['msg']['metadata']) : null
+              );
+              $eventQueue = CRM_Mailing_Event_BAO_Queue::create($params);
             }
+            switch ($value['event']) {
+            case 'open':
+              $oe                 = new CRM_Mailing_Event_BAO_Opened();
+              $oe->event_queue_id = $eventQueue->id;
+              $oe->time_stamp     = date('YmdHis', $value['ts']);
+              $oe->save();
+              break;
+              
+            case 'click':
+              $tracker = new CRM_Mailing_BAO_TrackableURL();
+              $tracker->url = $value['url'];
+              $tracker->mailing_id = $mail->id;
+              if (!$tracker->find(TRUE)) {
+                $tracker->save();
+              }
+              $open = new CRM_Mailing_Event_BAO_TrackableURLOpen();
+              $open->event_queue_id = $eventQueue->id;
+              $open->trackable_url_id = $tracker->id;
+              $open->time_stamp = date('YmdHis', $value['ts']);
+              $open->save();
+              break;
+              
+            case 'hard_bounce':
+            case 'soft_bounce':
+            case 'spam':
+            case 'reject':
+              if (empty($bounceType)) {
+                CRM_Core_PseudoConstant::populate($bounceType, 'CRM_Mailing_DAO_BounceType', TRUE, 'id', NULL, NULL, NULL, 'name');
+              }
             $bType = ucfirst(preg_replace('/_\w+/', '', $value['event']));
             $bounce             = new CRM_Mailing_Event_BAO_Bounce();
             $bounce->time_stamp =  date('YmdHis', $value['ts']);
@@ -98,11 +99,12 @@ class CRM_Mte_Page_callback extends CRM_Core_Page {
             $bounce->bounce_reason  = CRM_Core_DAO::getFieldValue('CRM_Mailing_DAO_BounceType', $bounceType["Mandrill $bType"], 'description');
             $bounce->save();
             break;
+            }
           }
         }
       }
     }
-    // TODO: replace the following with something that will end the session cleanly
-    // parent::run();
+   
+    CRM_Utils_System::civiExit();
   }
 }      
